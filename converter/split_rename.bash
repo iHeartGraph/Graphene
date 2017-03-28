6a014c1ae3d365a1f0a4f27d68c77114e8baf554
asherliu <asherliu@gwu.edu>
2017-03-28 09:17:19 -0400

    update converter and readme

---
+++ converter/split_rename.bash
@@ -0,0 +1,57 @@
+echo Input: /path/to/bash /path/to/file_to_be_split
+
+if [ ! $# -eq 1 ];
+then
+	echo Input wrong
+	exit
+fi
+
+echo $@
+
+file=$1
+
+if [ ! -f $file ];
+then
+	echo File **$file** does not exist
+	exit
+fi
+
+
+edge_count=$(wc -l $file | awk -F " " '{print $1}')
+file_count=200
+split_line=$((edge_count/file_count))
+
+
+###################
+echo Splitting the large file into ~200 smaller files 
+
+if [ ! $((split_line * file_count)) -eq $edge_count ]
+then
+    file_count=$((file_count+1))
+fi
+echo "File $file, split to $file_count files, each contains $split_line" 
+
+prefix=$file-split
+split -l $split_line "$file" $file
+
+
+#####################
+echo Renaming the split file
+
+m=0
+for r1 in {a..z};
+do
+	for r2 in {a..z};
+	do
+		name="$file""$r1""$r2"
+
+		if [ ! -f $name ];then
+			continue
+		fi
+		new="$prefix"-$(printf "%05d" $m)
+		echo mv $name $new
+		mv $name $new
+		m=$((m+1))
+	done
+done
+
---
+++ readme.md
@@ -0,0 +1,20 @@
+--
+Software requirement
+-----
+gcc 4.4.7 or 4.8.5
+
+
+--
+Project structure
+-----
+- converter: convert a text tuple list into **row-column balanced 2d partition** format of the graph.
+- Graphene contains **lib and test** two source code.
+    
+
+**Should you have any questions about this project, please contact us by iheartgraph@gmail.com.**
+
+--
+Reference
+-------
+[FAST '17] Graphene: Fine-Grained IO Management for Graph Computing[[PDF](https://www.usenix.org/system/files/conference/fast17/fast17-liu.pdf)] [[Slides](https://www.usenix.org/sites/default/files/conference/protected-files/fast17_slides_liu.pdf)]
+
